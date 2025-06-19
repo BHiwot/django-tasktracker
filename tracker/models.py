e25ff4e15dfb3d2351c9a03c750b0a54aff71d68
Hiwot Berhanu <hmmmfb@gmail.com>
2025-06-19 14:09:40 +0300

    feat(models):create Task model with initial migration

--- tracker/models.py
+++ tracker/models.py
@@ -18,4 +18,4 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now=True) #Automatically set on update
 
     def __str__(self):
-        return self.title
+        return self.title  # show title in admin
